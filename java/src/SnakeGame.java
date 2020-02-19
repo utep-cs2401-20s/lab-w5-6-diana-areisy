dd962ac203246246efa56d74db9cab0547a77836
diana-areisy <diareisy@gmail.com>
2020-02-19 14:49:56 -0700

    Started findTailExhaustive but it is NOT finished.

--- java/src/SnakeGame.java
+++ java/src/SnakeGame.java
@@ -20,7 +20,41 @@ public class SnakeGame {
         headPosition[1] = y;
     }
     //Methods
-    public int[] findTailExhaustive(){}
+    public int neighbors(int x, int y){
+        int neighborCount = 0;
+        if(game[x + 1][y]){
+            neighborCount++;
+        }
+        if(game[x-1][y]){
+            neighborCount++;
+        }
+        if(game[x][y+1]){
+            neighborCount++;
+        }
+        if(game[x][y-1]){
+            neighborCount++;
+        }
+        return neighborCount;
+    }
+    public int[] findTailExhaustive(){
+        int checks = 0;
+        int length = 0;
+        for(int i = 0; i < game.length; i++){
+            for(int j = 0; j < game.length; i++){
+                if(game[i][j]){
+                    length++;
+                    if(neighbors(i,j) == 1){
+                        if(i == headPosition[0] && j == headPosition[1] ){
+                            continue;
+                        }
+                        else if()
+                    }
+                    else if(neighbors(i,j) >= 2)
+                }
+                checks++;
+            }
+        }
+    }
     private int[] findTailRecursive(){}
     private int[] findTailRecursive(int[] currentPosition, int[] previousPosition){}
     private void resetCounters(){}

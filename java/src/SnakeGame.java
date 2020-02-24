c307efdc68bfccad012efc4a6e423661ce9ac7d4
diana-areisy <diareisy@gmail.com>
2020-02-24 14:24:08 -0700

    Finished finTailExhaustive method but has NOT been tested. Also completed resetCounters method..

--- java/src/SnakeGame.java
+++ java/src/SnakeGame.java
@@ -24,41 +24,78 @@ public class SnakeGame {
         int neighborCount = 0;
         if(game[x + 1][y]){
             neighborCount++;
+            boolean n = game[x+1][y];
         }
         if(game[x-1][y]){
             neighborCount++;
+            boolean n = game[x-1][y];
         }
         if(game[x][y+1]){
             neighborCount++;
+            boolean n = game[x][y+1];
         }
         if(game[x][y-1]){
             neighborCount++;
+            boolean n = game[x][y-1];
         }
         return neighborCount;
     }
-    public int[] findTailExhaustive(){
-        int checks = 0;
+    public int[] findTailExhaustive() {
+        resetCounters();
+
+        int x = 0;
+        int y = 0;
         int length = 0;
-        for(int i = 0; i < game.length; i++){
-            for(int j = 0; j < game.length; i++){
-                if(game[i][j]){
+        int[] taiLocation = new int[0];
+        boolean foundTail = false;
+
+        for (int i = 0; i < game.length; i++) {
+            for (int j = 0; j < game.length; i++) {
+                if (game[i][j]) {
                     length++;
-                    if(neighbors(i,j) == 1){
-                        if(i == headPosition[0] && j == headPosition[1] ){
+                    if (neighbors(i, j) == 1) {
+                        if (i == headPosition[0] && j == headPosition[1]) {
                             continue;
+                        } else {
+                            x = i;
+                            y = j;
+                            foundTail = true;
                         }
-                        else if()
                     }
-                    else if(neighbors(i,j) >= 2)
+                } else if (neighbors(i, j) >= 2) {
+                    continue;
+
                 }
-                checks++;
+                if(foundTail == true){
+                    exhaustiveChecks--;
+                }
+
             }
+            exhaustiveChecks++;
+
         }
+
+        return new int[]{x, y, length};
     }
-    private int[] findTailRecursive(){}
-    private int[] findTailRecursive(int[] currentPosition, int[] previousPosition){}
-    private void resetCounters(){}
-    private static int getRecursiveChecks(){}
-    private static int getExhaustiveChecks(){}
+
+    private int[] findTailRecursive(){
+        return new int[0];
+    }
+    private int[] findTailRecursive(int[] currentPosition, int[] previousPosition){
+        return currentPosition;
+    }
+
+    private void resetCounters(){
+        int exhaustiveChecks = 0;
+        int recursiveChecks = 0;
+    }
+    private static int getRecursiveChecks(){
+        return 0;
+        //NEEDS FIXING
+    }
+    private static int getExhaustiveChecks(){
+        return 0;
+        //NEEDS FIXING
+    }
 
 }
